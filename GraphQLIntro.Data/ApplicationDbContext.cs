a633b781776222f32a72cfb8219f7fdb4896c610
ikenna.ogbonna <ikenna.ogbonna@bankly.ng>
2023-04-06 10:29:36 +0100

    app DB context

--- GraphQLIntro.Data/ApplicationDbContext.cs
+++ GraphQLIntro.Data/ApplicationDbContext.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GraphQLIntro.Data.ContextConfigurations;
+using GraphQLIntro.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace GraphQLIntro.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
-        {}
+        { }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             var ids = new Guid[] { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() };
+            modelBuilder.ApplyConfiguration(new SuperheroContextConfiguration(ids));
+            modelBuilder.ApplyConfiguration(new SuperpowerContextConfiguration(ids));
+            modelBuilder.ApplyConfiguration(new MovieContextConfiguration(ids));
         }
+
+        public DbSet<SuperHeroe> SuperHeroes { get; set; }
+        public DbSet<Superpower> Superpowers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
     }
 }
+
